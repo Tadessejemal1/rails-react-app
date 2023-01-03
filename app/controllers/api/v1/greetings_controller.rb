591038b211ad2d6b2c0246a3fb008c71a873c587
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-03 11:31:01 +0300

    create message in the database

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     @greeting = Greeting.find(rand(1..5))
-    render json: @greeting
+    render json: { message: @greeting.name }
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (<h1>Hello World this!</h1>);
+  return (<h1>Hello Tadesse</h1>);
 }
 
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root'),
 );
